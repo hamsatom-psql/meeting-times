7e39b4fdb4de07f09556a34f3d455af3b708a5c9
Tomas Hamsa <okularek@centrum.cz>
2021-06-30 17:59:59 +0200

    Implement repository

--- src/main/java/org/repository/HashMapCalendarRepository.java
+++ src/main/java/org/repository/HashMapCalendarRepository.java
@@ -3,18 +3,22 @@ package org.repository;
 import org.model.Calendar;
 
 import javax.annotation.Nonnull;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
 public class HashMapCalendarRepository implements ICalendarRepository {
+    private final Map<UUID, Calendar> database = new HashMap<>();
+
     @Override
     public void save(@Nonnull Calendar calendar) {
-
+        database.put(calendar.getId(), calendar);
     }
 
     @Nonnull
     @Override
     public Optional<Calendar> selectOne(@Nonnull UUID id) {
-        return Optional.empty();
+        return Optional.ofNullable(database.get(id));
     }
 }

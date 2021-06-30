7e352d7962a3dcb8597977049c8da76a67c1d7c3
Tomas Hamsa <okularek@centrum.cz>
2021-06-30 22:32:05 +0200

    More day slot tests

--- src/test/java/org/model/DaySlotTypesTest.java
+++ src/test/java/org/model/DaySlotTypesTest.java
@@ -28,35 +28,39 @@ class DaySlotTypesTest {
     void setAvailableTypes() {
         daySlots.setAvailableTypes(START, END, TYPE);
         Assertions.assertTrue(daySlots.isPeriodAvailable(START, END));
+        Assertions.assertTrue(daySlots.isTypeAvailable(START, END, TYPE));
+    }
+
+    @Test
+    void setAvailableTypes_startBeforeEnd() {
+        Assertions.assertThrows(RuntimeException.class, () -> daySlots.setAvailableTypes(END, START, TYPE));
     }
 
     @Test
     void setAvailableTypes_subperiod() {
         daySlots.setAvailableTypes(START, END, TYPE);
         Assertions.assertTrue(daySlots.isPeriodAvailable(START.plusMinutes(20), END.minusMinutes(20)));
+        Assertions.assertTrue(daySlots.isTypeAvailable(START.plusMinutes(20), END.minusMinutes(20), TYPE));
     }
 
     @Test
     void setAvailableTypes_early() {
         daySlots.setAvailableTypes(START, END, TYPE);
         Assertions.assertFalse(daySlots.isPeriodAvailable(START.minusMinutes(1), END));
+        Assertions.assertFalse(daySlots.isTypeAvailable(START.minusMinutes(1), END, TYPE));
     }
 
     @Test
     void setAvailableTypes_late() {
         daySlots.setAvailableTypes(START, END, TYPE);
         Assertions.assertFalse(daySlots.isPeriodAvailable(START, END.plusMinutes(1)));
-    }
-
-    @Test
-    void setAvailableTypes_correctType() {
-        daySlots.setAvailableTypes(START, END, TYPE);
-        Assertions.assertTrue(daySlots.isTypeAvailable(START, END, TYPE));
+        Assertions.assertFalse(daySlots.isTypeAvailable(START, END.plusMinutes(1), TYPE));
     }
 
     @Test
     void setAvailableTypes_wrongType() {
         daySlots.setAvailableTypes(START, END, TYPE);
+        Assertions.assertTrue(daySlots.isPeriodAvailable(START, END));
         Assertions.assertFalse(daySlots.isTypeAvailable(START, END, UUID.randomUUID()));
     }
 
@@ -66,5 +70,20 @@ class DaySlotTypesTest {
         daySlots.takeSlots(START, END);
         Assertions.assertFalse(daySlots.isPeriodAvailable(START, END));
         Assertions.assertFalse(daySlots.isTypeAvailable(START, END, TYPE));
+    }
+
+    @Test
+    void takeSlots_startBeforeEnd() {
+        Assertions.assertThrows(RuntimeException.class, () -> daySlots.takeSlots(END, START));
+    }
+
+    @Test
+    void isPeriodAvailable_startBeforeEnd() {
+        Assertions.assertThrows(RuntimeException.class, () -> daySlots.isPeriodAvailable(END, START));
+    }
+
+    @Test
+    void isTypeAvailable_startBeforeEnd() {
+        Assertions.assertThrows(RuntimeException.class, () -> daySlots.isTypeAvailable(END, START, TYPE));
     }
 }

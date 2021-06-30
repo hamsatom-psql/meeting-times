c5c3abbc0ff6c0400d44b8fc27d53b3a0e4d2a93
Tomas Hamsa <okularek@centrum.cz>
2021-06-30 13:19:08 +0200

    Project skeleton

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    java
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.google.code.findbugs","jsr305", "3.0.2")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
